98acc8e7ecd0aaf9a2cb4d1a32988be47a777f9b
SarkhanS10 <sabirliserxan@gmail.com>
2024-07-14 19:58:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 int main()
 {
-int i;
-for ( i = 0; i < 32; i++)
-{
-printf(" Hello School\n");
+  int i;
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello School\n");
+  }
+  return 0;
 }
-return 0;
